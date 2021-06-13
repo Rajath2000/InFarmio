d793d0a87074a15a3f529d90e311d01f066f2f46
Rajath2000 <rajathmr2000@gmai.com>
2021-06-13 18:46:39 +0530

    SPLASH,LOGIN,SIGNUP Frontend with rediriting to different pages

--- app/src/main/java/com/example/infarmio/Login.java
+++ app/src/main/java/com/example/infarmio/Login.java
@@ -2,13 +2,37 @@ package com.example.infarmio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView loginText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginText=(TextView)findViewById(R.id.Login_redirecttosignup);
+
+
+
+
+
+
+
+
+        //Redirect to signup
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent=new Intent(Login.this,Signup.class);
+                startActivity(signUpIntent);
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/infarmio/Signup.java
+++ app/src/main/java/com/example/infarmio/Signup.java
@@ -2,13 +2,41 @@ package com.example.infarmio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
+    TextView loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //ALL objects Initialization
+        loginText=(TextView)findViewById(R.id.Signup_reditecttologin);
+
+
+
+
+
+
+
+
+
+
+
+        //Redirect to Loginpage
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent=new Intent(Signup.this,Login.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+
     }
 }
