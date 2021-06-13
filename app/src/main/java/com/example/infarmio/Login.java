52b17861d1f5461710432f5cba33ea44d53dbabc
Rajath2000 <rajathmr2000@gmai.com>
2021-06-13 21:34:14 +0530

    SPLASH,LOGIN,SIGNUP Frontend with rediriting to different pages

--- app/src/main/java/com/example/infarmio/Login.java
+++ app/src/main/java/com/example/infarmio/Login.java
@@ -4,17 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
     TextView loginText;
+    TextInputEditText userName,password;
+    Button login;
+
+    //declaring formvalidator objrct
+    AwesomeValidation awesomeValidation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+       //converting xml id to java objects
         loginText=(TextView)findViewById(R.id.Login_redirecttosignup);
+        userName=findViewById(R.id.Login_Email);
+        password=findViewById(R.id.Login_password);
+        login=findViewById(R.id.Login_Button);
+
+        //initalizing formvalidator objrct
+        //adding the style for validation
+        awesomeValidation=new AwesomeValidation(ValidationStyle.BASIC);
+
+        //add validation for Email
+        awesomeValidation.addValidation(this,R.id.layout_email, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+        awesomeValidation.addValidation(this,R.id.layout_password, RegexTemplate.NOT_EMPTY,R.string.invalid_password);
+
+        //Login button action
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(awesomeValidation.validate())
+                {
+                    Toast.makeText(Login.this, "Validation Sucessfull", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Login.this, "Please Enter the valid Details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/infarmio/Signup.java
+++ app/src/main/java/com/example/infarmio/Signup.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
+import java.util.regex.Pattern;
 
 public class Signup extends AppCompatActivity {
     TextView loginText;
-
+    Button Signup;
+    AwesomeValidation awesomeValidation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +28,35 @@ public class Signup extends AppCompatActivity {
 
         //ALL objects Initialization
         loginText=(TextView)findViewById(R.id.Signup_reditecttologin);
+        Signup=findViewById(R.id.SignUp_button);
+
+
+        //Validation Style
+        awesomeValidation=new AwesomeValidation(ValidationStyle.BASIC);
+        //Validation Rules
+        awesomeValidation.addValidation(this,R.id.Signup_layout_username, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+        awesomeValidation.addValidation(this,R.id.Signup_layout_phone,"[5-9]{1}[0-9]{9}$",R.string.invalid_phone);
+        awesomeValidation.addValidation(this,R.id.Signup_layout_password, RegexTemplate.NOT_EMPTY,R.string.invalid_password);
+        awesomeValidation.addValidation(this,R.id.Signup_layout_confirmpassword,R.id.Signup_layout_password,R.string.not_matching);
+
+
+        //signup button action
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(awesomeValidation.validate())
+                {
+
+                }
+                else
+                {
+                    Toast.makeText(Signup.this, "Enter Valid Details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
 
 
 
